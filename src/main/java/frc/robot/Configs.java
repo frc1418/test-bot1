cd4454e4ae2c4958ca909ef3e1c852fe3187c949
BenBerol <benberol40@gmail.com>
2025-01-10 16:26:06 -0500

    Swerve baseline development

---
+++ src/main/java/frc/robot/Configs.java
@@ -0,0 +1,43 @@
+package frc.robot;
+
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.EncoderConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.Constants.EncoderConstants;
+
+public final class Configs {
+     public static final class MAXSwerveModule {
+        public static final SparkMaxConfig speedConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig angleConfig = new SparkMaxConfig();
+
+        static {
+            speedConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(50);
+            speedConfig.encoder
+                .positionConversionFactor(EncoderConstants.ROTATIONS_TO_METERS)
+                .velocityConversionFactor(EncoderConstants.ROTATIONS_TO_METERS/60.0);
+
+            speedConfig.closedLoop
+                .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                .pid(1.0, 0.0, 0.0)
+                .velocityFF(0.25);
+
+            angleConfig
+                .idleMode(IdleMode.kBrake)
+                .smartCurrentLimit(20);
+            angleConfig.absoluteEncoder
+                .inverted(true)
+                .positionConversionFactor(EncoderConstants.TURNING_FACTOR)
+                .velocityConversionFactor(EncoderConstants.TURNING_FACTOR/60.0);
+            angleConfig.closedLoop
+                .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
+                .pid(1.75,0,0)
+                .outputRange(-1, 1)
+                .positionWrappingEnabled(true)
+                .positionWrappingInputRange(0, EncoderConstants.TURNING_FACTOR);
+        }
+    }
+}

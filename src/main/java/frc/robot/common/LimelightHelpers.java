ff53ae740f65a3e0d041a0c9cea832a105d38c35
BenBerol <benberol40@gmail.com>
2025-01-18 11:12:54 -0500

    Create swerve drive pose estimator and support class

---
+++ src/main/java/frc/robot/common/LimelightHelpers.java
@@ -0,0 +1,140 @@
+package frc.robot.common;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.DoubleArrayEntry;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.TimestampedDoubleArray;
+
+public class LimelightHelpers {
+
+    public static class RawFiducial {
+        public int id = 0;
+        public double txnc = 0;
+        public double tync = 0;
+        public double ta = 0;
+        public double distToCamera = 0;
+        public double distToRobot = 0;
+        public double ambiguity = 0;
+
+
+        public RawFiducial(int id, double txnc, double tync, double ta, double distToCamera, double distToRobot, double ambiguity) {
+            this.id = id;
+            this.txnc = txnc;
+            this.tync = tync;
+            this.ta = ta;
+            this.distToCamera = distToCamera;
+            this.distToRobot = distToRobot;
+            this.ambiguity = ambiguity;
+        }
+    }
+
+    public static class PoseEstimate {
+        public Pose2d pose;
+        public double timestampSeconds;
+        public double latency;
+        public int tagCount;
+        public double tagSpan;
+        public double avgTagDist;
+        public double avgTagArea;
+        public RawFiducial[] rawFiducials; 
+
+        public PoseEstimate() {
+            this.pose = new Pose2d();
+            this.timestampSeconds = 0;
+            this.latency = 0;
+            this.tagCount = 0;
+            this.tagSpan = 0;
+            this.avgTagDist = 0;
+            this.avgTagArea = 0;
+            this.rawFiducials = new RawFiducial[]{};
+        }
+
+        public PoseEstimate(Pose2d pose, double timestampSeconds, double latency, 
+            int tagCount, double tagSpan, double avgTagDist, 
+            double avgTagArea, RawFiducial[] rawFiducials) {
+
+            this.pose = pose;
+            this.timestampSeconds = timestampSeconds;
+            this.latency = latency;
+            this.tagCount = tagCount;
+            this.tagSpan = tagSpan;
+            this.avgTagDist = avgTagDist;
+            this.avgTagArea = avgTagArea;
+            this.rawFiducials = rawFiducials;
+        }
+    }
+
+    public static PoseEstimate getBotPoseEstimate() {
+        DoubleArrayEntry poseEntry = LimelightHelpers.getLimelightDoubleArrayEntry("limelight", "wpiBlue_MegaTag2");
+        
+        TimestampedDoubleArray tsValue = poseEntry.getAtomic();
+        double[] poseArray = tsValue.value;
+        long timestamp = tsValue.timestamp;
+        var pose = toPose2D(poseArray);
+
+        
+        if (poseArray.length == 0) {
+            // Handle the case where no data is available
+            return null; // or some default PoseEstimate
+        }
+    
+        double latency = extractArrayEntry(poseArray, 6);
+        int tagCount = (int)extractArrayEntry(poseArray, 7);
+        double tagSpan = extractArrayEntry(poseArray, 8);
+        double tagDist = extractArrayEntry(poseArray, 9);
+        double tagArea = extractArrayEntry(poseArray, 10);
+        
+        // Convert server timestamp from microseconds to seconds and adjust for latency
+        double adjustedTimestamp = (timestamp / 1000000.0) - (latency / 1000.0);
+    
+        RawFiducial[] rawFiducials = new RawFiducial[tagCount];
+        int valsPerFiducial = 7;
+        int expectedTotalVals = 11 + valsPerFiducial * tagCount;
+    
+        if (poseArray.length != expectedTotalVals) {
+            // Don't populate fiducials
+        } 
+        else {
+            for(int i = 0; i < tagCount; i++) {
+                int baseIndex = 11 + (i * valsPerFiducial);
+                int id = (int)poseArray[baseIndex];
+                double txnc = poseArray[baseIndex + 1];
+                double tync = poseArray[baseIndex + 2];
+                double ta = poseArray[baseIndex + 3];
+                double distToCamera = poseArray[baseIndex + 4];
+                double distToRobot = poseArray[baseIndex + 5];
+                double ambiguity = poseArray[baseIndex + 6];
+                rawFiducials[i] = new RawFiducial(id, txnc, tync, ta, distToCamera, distToRobot, ambiguity);
+            }
+        }
+    
+        return new PoseEstimate(pose, adjustedTimestamp, latency, tagCount, tagSpan, tagDist, tagArea, rawFiducials);
+    }
+
+        private static DoubleArrayEntry getLimelightDoubleArrayEntry(String tableName, String entryName) {
+            return NetworkTableInstance.getDefault().getTable(tableName).getDoubleArrayTopic(entryName).getEntry(new double[0]);
+        }
+
+        private static double extractArrayEntry(double[] inData, int position){
+            if(inData.length < position+1)
+            {
+                return 0;
+            }
+            return inData[position];
+        }
+
+        private static Pose2d toPose2D(double[] inData){
+            if(inData.length < 6)
+            {
+                return new Pose2d();
+            }
+            Translation2d tran2d = new Translation2d(inData[0], inData[1]);
+            Rotation2d r2d = new Rotation2d(Units.degreesToRadians(inData[6]));
+            return new Pose2d(tran2d, r2d);
+    }
+    
+    }
--- src/main/java/frc/robot/common/Odometry.java
+++ src/main/java/frc/robot/common/Odometry.java
@@ -2,32 +2,46 @@ package frc.robot.common;
 
 import com.studica.frc.AHRS;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 //import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.common.LimelightHelpers;
 
 
 
 public class Odometry extends SubsystemBase{
-    private final SwerveDriveOdometry swerveOdometry;
+    private final NetworkTableInstance ntInstance = NetworkTableInstance.getDefault();
+    private final NetworkTable table = ntInstance.getTable("/components/odometry");
+
+    private final NetworkTableEntry robot_orientation = table.getEntry("robot_orientation");
+
     private final AHRS gyro;
-    private SwerveModulePosition[] modulePositions;
 
     private Pose2d pose;
 
+    private boolean rejectVision = false;
+
+    private final SwerveDrivePoseEstimator poseEstimator;
+
     public Odometry(SwerveModulePosition[] modulePositions) {
         this.gyro = new AHRS(AHRS.NavXComType.kMXP_SPI);
-        this.swerveOdometry = new SwerveDriveOdometry(DrivetrainConstants.SWERVE_KINEMATICS, getHeading(), modulePositions);
-        this.modulePositions = modulePositions;
         this.pose = new Pose2d();
+        this.poseEstimator = new SwerveDrivePoseEstimator(
+            DrivetrainConstants.SWERVE_KINEMATICS, getHeading(), modulePositions, pose,
+            VecBuilder.fill(0.05, 0.05, Units.degreesToRadians(5)),
+            VecBuilder.fill(0.5, 0.5, Units.degreesToRadians(30)));
     }
 
     public void zeroHeading() {
@@ -35,14 +49,61 @@ public class Odometry extends SubsystemBase{
     }
 
     public Rotation2d getHeading() {
-        return Rotation2d.fromDegrees(gyro.getAngle()).unaryMinus();
+        return gyro.getRotation2d();
     }
 
     public void update(SwerveModulePosition[] modulePositions) {
-        swerveOdometry.update(getHeading(), modulePositions);
+        rejectVision = false;
+
+        poseEstimator.update(getHeading(), modulePositions);
+
+        setRobotOrientation(gyro.getAngle(), gyro.getRate(), gyro.getPitch(), 0, gyro.getRoll(), 0);
+        LimelightHelpers.PoseEstimate aprilTagInfo = LimelightHelpers.getBotPoseEstimate();
+
+        if (aprilTagInfo != null) {
+            if (aprilTagInfo.rawFiducials.length == 1) {
+                double ambiguity = aprilTagInfo.rawFiducials[0].ambiguity;
+                if (ambiguity > 0.9) {
+                    rejectVision = true;
+                }
+            }
+            else if (gyro.getRate() > 720) {
+                rejectVision = true;
+            }
+            else if (aprilTagInfo.tagCount == 0) {
+                rejectVision = true;
+            }
+        }
+        else {
+            rejectVision = true;
+        }
+
+        if (!rejectVision) {
+            System.out.println("X (m): " + aprilTagInfo.pose.getX());
+            System.out.println("Y (m): " + aprilTagInfo.pose.getY());
+            System.out.println("Rot (degrees): " + aprilTagInfo.pose.getRotation().getDegrees());
+            System.out.println("Time (sec): " + aprilTagInfo.timestampSeconds);
+            poseEstimator.addVisionMeasurement(aprilTagInfo.pose, aprilTagInfo.timestampSeconds);
+        }
+    }
+
+    private void setRobotOrientation (
+        double yaw, double yawRate, 
+        double pitch, double pitchRate, 
+        double roll, double rollRate) {
+
+        double[] entries = new double[6];
+        entries[0] = yaw;
+        entries[1] = yawRate;
+        entries[2] = pitch;
+        entries[3] = pitchRate;
+        entries[4] = roll;
+        entries[5] = rollRate;
+
+        robot_orientation.setDoubleArray(entries);
     }
 
     public void resetOdometry(Pose2d pose, SwerveModulePosition[] modulePositions) {
-        swerveOdometry.resetPosition(getHeading(), modulePositions, pose);
+        poseEstimator.resetPosition(getHeading(), modulePositions, pose);
     }
 }

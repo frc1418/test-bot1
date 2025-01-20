98f9092cebf4f69486ab8ca85355ffdc95c46132
Alecm37 <145499415+Alecm37@users.noreply.github.com>
2025-01-20 11:31:19 -0500

    https://github.com/frc1418/frc1418.github.io.git

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,27 +10,29 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 public final class Constants {
   public final static class DrivetrainConstants{
 
+    public static final double wheelPos = 0.2978; //previous value: 0.238125
+
     public static final double DRIFT_DEADBAND = 0.1;
     public static final double ROTATION_DEADBAND = 0.05;
 
     public static final int BACK_RIGHT_ANGLE_ID = 7;
     public static final int BACK_RIGHT_SPEED_ID = 8;
-    public static final Translation2d BACK_RIGHT_LOC = new Translation2d(-0.238125, -0.238125);
+    public static final Translation2d BACK_RIGHT_LOC = new Translation2d(-wheelPos, -wheelPos);
     public static final double BACK_RIGHT_ENCODER_OFFSET = 0.631-Math.PI/2;
 
     public static final int FRONT_RIGHT_ANGLE_ID = 5;
     public static final int FRONT_RIGHT_SPEED_ID = 6;
-    public static final Translation2d FRONT_RIGHT_LOC = new Translation2d(0.238125, -0.238125);
+    public static final Translation2d FRONT_RIGHT_LOC = new Translation2d(wheelPos, -wheelPos);
     public static final double FRONT_RIGHT_ENCODER_OFFSET = 3.672;
     
     public static final int BACK_LEFT_ANGLE_ID = 1;
     public static final int BACK_LEFT_SPEED_ID = 2;
-    public static final Translation2d BACK_LEFT_LOC = new Translation2d(-0.238125, 0.238125);
+    public static final Translation2d BACK_LEFT_LOC = new Translation2d(-wheelPos, wheelPos);
     public static final double BACK_LEFT_ENCODER_OFFSET = 5.339;
     
     public static final int FRONT_LEFT_ANGLE_ID = 3;
     public static final int FRONT_LEFT_SPEED_ID = 4;
-    public static final Translation2d FRONT_LEFT_LOC = new Translation2d(0.238125, 0.238125);
+    public static final Translation2d FRONT_LEFT_LOC = new Translation2d(wheelPos, wheelPos);
     public static final double FRONT_LEFT_ENCODER_OFFSET = 3.783+Math.PI/2;
     
     public static final SwerveDriveKinematics SWERVE_KINEMATICS = new SwerveDriveKinematics(
